e21101644387356d25d3b1e1bf51628ef472e724
SofRahmani <sofiane.rahmani12@gmail.com>
2024-02-18 17:04:45 +0100

    first commit

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,60 @@
+import { Locale, i18n } from "@/i18n.config";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+import { Metadata } from "next";
+import { fontSans } from "../../config/fonts";
+import { siteConfig } from "../../config/site";
+import "../../styles/globals.css";
+import { Navbar } from "./components/navbar";
+import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`
+  },
+  description: siteConfig.description,
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png"
+  }
+};
+
+export async function generateStaticParams() {
+  return i18n.locales.map(locale => ({ lang: locale }));
+}
+
+export default function RootLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: { lang: Locale };
+}) {
+  return (
+    <html lang={params.lang} suppressHydrationWarning>
+      <head />
+      <body className={clsx("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex min-h-screen dark:bg-[#1b1b1f] flex-col">
+            <Navbar lang={params.lang} />
+            <main className="container mx-auto max-w-7xl flex-grow px-6 pt-16">{children}</main>
+            <footer className="flex w-full items-center justify-center py-3">
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+                title="nextui.org homepage"
+              >
+                <span className="text-default-600">Powered by</span>
+                <p className="text-primary">NextUI</p>
+              </Link>
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Template Sofiane",
+	description: "Home make template with Next JS, Next UI, i18n, and more",
+	links: {
+		github: "https://github.com/nextui-org/nextui",
+		twitter: "https://twitter.com/getnextui",
+		docs: "https://nextui.org",
+		discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev"
+	},
+};
