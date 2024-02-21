b78efdbe9a89afa775ea8c0544c2d39615fc7ded
SofRahmani <sofiane.rahmani12@gmail.com>
2024-02-21 23:50:18 +0100

    refactor: :recycle: site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,9 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Letter or Number?",
+	name: "Letter or Number",
 	description: "A simpler app to help you decide if a character is a letter or a number.",
 	links: {
-		github: "https://github.com/SofRahmani/Letter-or-Number",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui.org",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
+		github: "https://github.com/SofRahmani/Letter-or-Number"
 	},
 };
