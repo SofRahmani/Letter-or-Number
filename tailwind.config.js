8762590cbf3d1520ddc141c1b38596a06b04a599
SofRahmani <sofiane.rahmani12@gmail.com>
2024-02-21 23:21:36 +0100

    feat: :lipstick: add spotlight animation on Home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+    }
   },
   darkMode: "class",
   plugins: [
