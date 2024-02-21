af2e235a66ff3fa4a0f68b767d4a413a60d31771
SofRahmani <sofiane.rahmani12@gmail.com>
2024-02-21 21:29:09 +0100

    refactor: :recycle: toUpperCase language button

--- src/app/[lang]/components/navbar/ToggleLaguage.tsx
+++ src/app/[lang]/components/navbar/ToggleLaguage.tsx
@@ -25,7 +25,7 @@ export function ToggleLaguage() {
               size="sm"
               variant="bordered"
             >
-              {locale}
+              {locale.toUpperCase()}
             </Button>
           </Link>
         );
